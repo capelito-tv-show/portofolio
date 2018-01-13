04ce4b5e5121a8fbbdc1df6ddcbca546d512384d
[capelito-tv-show] <capelito.tv.show@gmail.com>
2018-01-13 18:33:27 +0900

    first commit

---
+++ src/App/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDIGyiMrucceVBxRTeJi9hp0p6fw9awb0o",
+  authDomain: "auth-d8669.firebaseapp.com",
+  databaseURL: "https://auth-d8669.firebaseio.com",
+  projectId: "auth-d8669",
+  storageBucket: "auth-d8669.appspot.com",
+  messagingSenderId: "589582202485"
+};
---
+++ src/containers/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+
+import config from '../App/firebase-config';
+
+class App extends Component {
+  constructor() {
+    super();
+    // Initialize Firebase
+    firebase.initializeApp(config);
+  }
+
+  state = {
+    posts: [],
+    loading: true
+  };
+
+
+  componentWillMount() {
+    let postsRef = firebase.database().ref('posts');
+
+    let _this = this;
+
+    postsRef.on('value', function(snapshot) {
+      console.log(snapshot.val());
+
+      _this.setState({
+        posts: snapshot.val(),
+        loading: false
+      });
+    });
+
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <p className="App-intro">
+          {this.props.children && React.cloneElement(
+            this.props.children, {
+              firebase: firebase.database(),
+              posts: this.state.posts,
+              loading: this.state.loading
+          })}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
